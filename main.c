8b90c90f15d9e6efc41dc527b9b2adee9a98c7a1
minseo0520 <141472390+minseo0520@users.noreply.github.com>
2023-11-09 15:53:38 +0900

    실습5

--- main.c
+++ main.c
@@ -3,13 +3,26 @@
 #include <string.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-void main(void){
-	char src[] = "The worst things to eat before you sleep";
-	char dst[100];
-		
-	/*strcpy*/
-	strcpy(dst, src);
+int main(void){
+	FILE*fp;
+	char str[100];
+	int i;
 	
-	printf("copied string : %s\n", dst);
+	//1. open file
+	fp=fopen("sample.txt", "w");
+	
+	//2.write file
+	for(i=0; i<3; i++)
+	{
+	   //2-1. print. input a word
+	    printf("input a word : ");
+        //2-2. scanf a string
+	    scanf("%s", str);
+	    //2-3. fprintf()
+	    fprintf(fp, "%s\n", str);  
+	}
+	//3. close file
+	fclose(fp);
+	
+	return 0;
 }
- 
